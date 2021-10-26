6c3480fb892a0652dd9dae62953124e334b44bd2
fengqilove520 <1309012576@qq.com>
2021-10-26 17:01:55 +0800

    区域信息扩展

--- src/main/java/top/fqq/familytree/bean/dto/person/PersonDto.java
+++ src/main/java/top/fqq/familytree/bean/dto/person/PersonDto.java
@@ -18,19 +18,68 @@ public class PersonDto extends BaseDto {
     private String id;
 
     /**
+     * 姓氏
+     */
+    private String familyName;
+
+    /**
      * 名称
      */
     private String name;
+
+
+    /**
+     * 辈分
+     */
+    private String generation;
 
     /**
      * 性别
      */
     private Boolean sex;
+    /**
+     * 户籍省份
+     */
+    private String domicileProvince;
 
+    /**
+     * 户籍城市
+     */
+    private String domicileCity;
+
+    /**
+     * 户籍县城
+     */
+    private String domicileCounty;
+
+    /**
+     * 户籍城镇
+     */
+    private String domicileTown;
     /**
      * 户籍地址
      */
     private String domicilePlace;
+
+    /**
+     * 居住省份
+     */
+    private String residentialProvince;
+
+    /**
+     * 居住城市
+     */
+    private String residentialCity;
+
+    /**
+     * 居住县城
+     */
+    private String residentialCounty;
+
+    /**
+     * 居住乡镇
+     */
+    private String residentialTown;
 
     /**
      * 居住地址
